481417eef917ee813b7f55f59091f3d9021e3f68
Pri Patel <priyankp295@gmail.com>
2025-09-07 17:51:14 -0400

    update navbar with temporary logo.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -19,7 +19,9 @@ const NavigationBar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark custom-bg text-white">
       <div className="container-fluid">
-        <Link className="navbar-brand" to="/">React App</Link>
+        <Link className="navbar-brand" to="/">
+          <img src="favicon.ico" alt="Logo" width="30" height="30" className="d-inline-block align-text-top me-2" />
+        </Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
